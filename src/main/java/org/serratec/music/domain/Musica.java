d0b9a1e007507de51899e0cc1654d9c1a02aa1f0
AmandaLisboa-Ramos <amandalisboa.am@gmail.com>
2025-10-17 11:55:04 -0300

    entrega dos pacotes domain

--- src/main/java/org/serratec/music/domain/Musica.java
+++ src/main/java/org/serratec/music/domain/Musica.java
@@ -1,5 +1,97 @@
 package org.serratec.music.domain;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Musica")
 public class Musica {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "titulo")
+	@NotBlank(message = "O campo Titulo não pode estar em branco")
+	@Size(max = 100, message = "O campo Titulo não pode estar em branco")
+	private String titulo;
+
+	@Column(name = "minutos")
+	@NotNull(message = "O campo Minutos não pode estar em branco")
+	@Max(value = 4, message = "O campo Minutos não pode ter mais de 4 números")
+	private Integer minutos;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	@NotNull(message = "o Campo genero não estar em branco")
+	private GeneroMusical genero;
+	
+	@ManyToMany
+	@JoinTable(
+	name = "musica_artista",
+	joinColumns = @JoinColumn(name = "musica_id"),
+	inverseJoinColumns = @JoinColumn(name = "artista_id"))
+	private List<Artista> artistas;
+	
+	@ManyToMany(mappedBy = "musicas")
+	private List<Playlist> playlists;
+
+	public Musica(Long id, String titulo, Integer minutos, GeneroMusical genero) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.minutos = minutos;
+		this.genero = genero;
+	}
+
+	public Musica() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(Integer minutos) {
+		this.minutos = minutos;
+	}
+
+	public GeneroMusical getGenero() {
+		return genero;
+	}
+
+	public void setGenero(GeneroMusical genero) {
+		this.genero = genero;
+	}
 }
--- src/main/java/org/serratec/music/domain/Playlist.java
+++ src/main/java/org/serratec/music/domain/Playlist.java
@@ -1,5 +1,87 @@
 package org.serratec.music.domain;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Playlist")
 public class Playlist {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nome")
+	@NotBlank(message = "O campo Nome não pode estar em branco")
+	@Size(max = 80, message = "O campo Nome não pode ter mais de 80 caracteres")
+	private String nome;
+
+	@Column(name = "descricao")
+	@NotBlank(message = "O campo Descriçao não pode estar em branco")
+	@Size(max = 200, message = "o campo Descrição não pode ter mais de 200 caracteres.")
+	private String descricao;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
+
+	@ManyToMany
+	@JoinTable(name = "playlist_musica", joinColumns = @JoinColumn(name = "playlist_id"), inverseJoinColumns = @JoinColumn(name = "musica_id"))
+	private List<Musica> musicas;
+
+	public Playlist(Long id, String nome, String descricao, List<Musica> musicas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.musicas = musicas;
+	}
+
+	public Playlist() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Musica> getMusicas() {
+		return musicas;
+	}
+
+	public void setMusicas(List<Musica> musicas) {
+		this.musicas = musicas;
+	}
 }
